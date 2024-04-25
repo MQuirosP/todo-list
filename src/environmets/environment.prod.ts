ed2d8644eb830e1b3420fbdd3ebd5d56bcf30036
Mario Quirós <115672555+MQuirosP@users.noreply.github.com>
2024-04-25 10:19:20 -0600

    modified:   .gitignore 	deleted:    src/environmets/environment.prod.ts 	deleted:    src/environmets/environment.ts

--- src/environmets/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-
-  
-  // src/environments/environment.prod.ts
-  export const environment = {
-    production: true,
-    openWeatherMapApiKey: '5c13d9881911979dc57640a266c7bfd5'
-  };
-  
--- src/environmets/environment.ts
+++
@@ -1,5 +0,0 @@
-// src/environments/environment.ts
-export const environment = {
-    production: false,
-    openWeatherMapApiKey: '5c13d9881911979dc57640a266c7bfd5'
-  };
