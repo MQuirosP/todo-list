0c64a1c8f87609e529697d96b8095391d20c6aaf
Mario Quirós <115672555+MQuirosP@users.noreply.github.com>
2024-04-27 11:37:13 -0600

    deleted:    src/app/shared/pipes/format-date.pipe.spec.ts 	modified:   src/app/shared/transition.ts

--- src/app/shared/transition.ts
+++ src/app/shared/transition.ts
@@ -15,11 +15,11 @@ import {
       group([
         query(':enter', [
           style({ opacity: 0 }),
-          animate('0.3s ease-in-out', style({ opacity: 1 }))
+          animate('0.3s ease', style({ opacity: 1 }))
         ], { optional: true }),
         query(':leave', [
           style({ opacity: 1 }),
-          animate('0.3s ease-in-out', style({ opacity: 0 }))
+          animate('0.3s ease', style({ opacity: 0 }))
         ], { optional: true })
       ])
     ])
